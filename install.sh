829bb0d1e9aa1f50521b6ad7c7e1c15645f7db3e
Mark de Groot <markdegroot@kpn.com>
2017-10-24 07:45:59 +0200

    Add install.sh

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+
+## Installing core packages
+
+sudo apt-get install build-essential libreadline-dev libssl-dev libpq5 libpq-dev libreadline5 libsqlite3-dev libpcap-dev git-core autoconf postgresql pgadmin3 curl zlib1g-dev libxml2-dev libxslt1-dev tigervnc-viewer libyaml-dev curl zlib1g-dev build-essential libreadline-dev libssl-dev libpq5 libpq-dev libreadline5 libsqlite3-dev libpcap-dev git-core autoconf postgresql pgadmin3 curl zlib1g-dev libxml2-dev libxslt1-dev tigervnc-viewer libyaml-dev curl zlib1g-dev git git-cola checkinstall cvs subversion git-core mercurial manpages-dev man-db manpages-posix-dev
+
